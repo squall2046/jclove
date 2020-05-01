3d0039e37fe568079b42b7733b602a0d77a004dc
squall2046 <squall2046@outlook.com>
2020-05-01 02:06:25 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,29 +4,29 @@ const path = require('path');
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
-const cors = require("cors");
-app.use(cors());
-// Add headers (access control 允许跨域)
-// =============================================================
-app.all("*", function (req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    // Request methods you wish to allow
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-    );
-    // Request headers you wish to allow
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "X-Requested-With,content-type"
-    );
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    // Pass to next layer of middleware
-    next();
-});
+// const cors = require("cors");
+// app.use(cors());
+// // Add headers (access control 允许跨域)
+// // =============================================================
+// app.all("*", function (req, res, next) {
+//     // Website you wish to allow to connect
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     // Request methods you wish to allow
+//     res.setHeader(
+//         "Access-Control-Allow-Methods",
+//         "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//     );
+//     // Request headers you wish to allow
+//     res.setHeader(
+//         "Access-Control-Allow-Headers",
+//         "X-Requested-With,content-type"
+//     );
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader("Access-Control-Allow-Credentials", true);
+//     // Pass to next layer of middleware
+//     next();
+// });
 
 app.use(express.static(__dirname + '/dist/jclove'));
 app.use(express.urlencoded({ extended: true }));
@@ -39,13 +39,13 @@ app.get('*', function (req, res) {
 
 // Routes 放在最后，且在listen port前。
 // =============================================================
-require(path.join(__dirname, "/routes/apiRoutes.js"))(app);
+// require(path.join(__dirname, "/routes/apiRoutes.js"))(app);
 
 // PORT
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, "127.0.0.1", () => console.log("App running on port " + PORT + "!"));
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, "127.0.0.1", () => console.log("App running on port " + PORT + "!"));
 
-// app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080);
 
 // npx nodemon 开始程序
 // node server 也可以, 但是每次更新要通过ctrl+c,并重开server才能显示更新内容.
