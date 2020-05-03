f951b13f6e8a3260766fdbbdbb736be04b736404
squall2046 <squall2046@outlook.com>
2020-05-03 03:51:49 -0400

    commented cors in server

--- server.js
+++ server.js
@@ -4,31 +4,31 @@ const path = require('path');
 const bodyParser = require('body-parser')
 app.use(bodyParser.json());
 
-var mongodb = require("mongodb");
+// var mongodb = require("mongodb");
 
-const cors = require("cors");
-app.use(cors());
-// Add headers (access control 允许跨域)
-// =============================================================
-app.all("*", function (req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    // Request methods you wish to allow
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-    );
-    // Request headers you wish to allow
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "X-Requested-With,content-type"
-    );
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    // Pass to next layer of middleware
-    next();
-});
+// const cors = require("cors");
+// app.use(cors());
+// // Add headers (access control 允许跨域)
+// // =============================================================
+// app.all("*", function (req, res, next) {
+//     // Website you wish to allow to connect
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     // Request methods you wish to allow
+//     res.setHeader(
+//         "Access-Control-Allow-Methods",
+//         "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//     );
+//     // Request headers you wish to allow
+//     res.setHeader(
+//         "Access-Control-Allow-Headers",
+//         "X-Requested-With,content-type"
+//     );
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader("Access-Control-Allow-Credentials", true);
+//     // Pass to next layer of middleware
+//     next();
+// });
 
 app.use(express.static(__dirname + '/dist/jclove'));
 app.use(express.urlencoded({ extended: true }));
